6451b8285066fb8aaadac01533ff5a641d718629
sonu <8347goswamisonu@gmail.com>
2021-03-28 16:20:13 +0530

    magic ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Center(child: Text('Ask ME Anything')),
+        backgroundColor: Colors.blue,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int BallNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            BallNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$BallNumber.png'),
+      ),
+    );
+  }
+}
